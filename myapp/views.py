64711b9ba677783b55f468234ce6408fe57a58a0
soyotrokevin <soyotrokevin@gmail.com>
2022-08-27 16:55:27 -0300

    Modificación estética

--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,7 @@ from myapp.models import Familia
 
 def mi_familia (request):
     queryset = Familia.objects.all
-    diccionario = {'miapp':queryset}
+    diccionario = {'flia':queryset}
     plantilla = loader.get_template('lista_flia.html')
     doc_html=plantilla.render(diccionario)
 
