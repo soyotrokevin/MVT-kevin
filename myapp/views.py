3767d33e7a52e200a876dded3f231f47d3f5a9c2
soyotrokevin <soyotrokevin@gmail.com>
2022-08-27 16:24:59 -0300

    Carga de modelos y datos

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template, Context, loader
+from myapp.models import Familia
 
-# Create your views here.
+def mi_familia (request):
+    queryset = Familia.objects.all
+    diccionario = {'miapp':queryset}
+    plantilla = loader.get_template('lista_flia.html')
+    doc_html=plantilla.render(diccionario)
+
+    return HttpResponse(doc_html)
